79d261221e7adb69381cf3b678dfe941f6032c27
Tony Lofthouse <a0741364@ti.com>
2012-03-05 14:15:18 -0600

    omap: remove unused omaplfb platform device
    Change-Id: Ic7d8aa22868c0cf2396ae499a1a320903e9feb31
    Signed-off-by: Tony Lofthouse <a0741364@ti.com>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -956,12 +956,6 @@ static struct omap_device_pm_latency omap_gpu_latency[] = {
 	},
 };
 
-static struct platform_device omap_omaplfb_device = {
-	.name		= "omaplfb",
-	.id		= -1,
-};
-
-
 static void omap_init_gpu(void)
 {
 	struct omap_hwmod *oh;
@@ -1010,7 +1004,6 @@ static void omap_init_gpu(void)
 	     name, oh_name);
 
 	kfree(pdata);
-	platform_device_register(&omap_omaplfb_device);
 }
 
 /*-------------------------------------------------------------------------*/

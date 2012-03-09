b545af4816d06dba671b5256506cdd2fc19fed10
Lajos Molnar <lajos@ti.com>
2012-03-08 19:29:37 -0600

    OMAP4: ion: Calculate carveout addresses based on sizes
    Calculate carveout addresses somewhat dynamically.
    
    - calculate the carveout addresses based on starting or ending
    address and size.
    
    - get the tiler2D and nonsecure tiler2D sizes from a separate
    platform config variable
    
    - lay out nonsecure tiler2D carveout before tiler2D carvout.
    This allows only aligning the sum of these two carveout to
    2MB, instead of the size of each.
    
    Change-Id: I38c12459e6229ab9ef2deb89c0bce712e424a2a9
    Signed-off-by: Lajos Molnar <lajos@ti.com>
    Signed-off-by: Dima Svetlov <svetlov@ti.com>
    
    Conflicts:
    	arch/arm/mach-omap2/include/mach/omap4_ion.h

--- arch/arm/mach-omap2/include/mach/omap4_ion.h
+++ arch/arm/mach-omap2/include/mach/omap4_ion.h
@@ -19,6 +19,8 @@
 
 #define OMAP4_RAMCONSOLE_START	(PLAT_PHYS_OFFSET + SZ_512M)
 #define OMAP4_RAMCONSOLE_SIZE	SZ_2M
+#include <linux/ion.h>
+
 #define OMAP4_ION_HEAP_SECURE_INPUT_SIZE	(SZ_1M * 90)
 #define OMAP4_ION_HEAP_TILER_SIZE		(SZ_128M - SZ_32M)
 #define OMAP4_ION_HEAP_NONSECURE_TILER_SIZE	SZ_32M
@@ -35,6 +37,15 @@
 					OMAP4_ION_HEAP_TILER_SIZE - \
 					PHYS_ADDR_TESLA_SIZE)
 #endif
+
+
+struct omap_ion_platform_data {
+	struct ion_platform_data *ion;
+	u32 tiler2d_size;
+	u32 nonsecure_tiler2d_size;
+};
+
+struct omap_ion_platform_data *get_omap_ion_platform_data(void);
 
 #ifdef CONFIG_ION_OMAP
 void omap_ion_init(void);

ff851e76b3d3bbdc0d5f7a52e3e46f8f4d52057f
Arve Hjønnevåg <arve@android.com>
2012-09-26 18:01:17 -0700

    staging: android: lowmemorykiller: Don't count reserved free memory
    The amount of reserved memory varies between devices. Subtract it
    here to reduce the amount of devices specific tuning needed for the
    minfree values.
    
    Change-Id: I466ae8b18f5972f6f6d8b5a7d8c4ae69660de53a
    Signed-off-by: Arve Hjønnevåg <arve@android.com>

--- drivers/staging/android/lowmemorykiller.c
+++ drivers/staging/android/lowmemorykiller.c
@@ -132,7 +132,7 @@ static int lowmem_shrink(struct shrinker *s, struct shrink_control *sc)
 	int selected_tasksize = 0;
 	int selected_oom_adj;
 	int array_size = ARRAY_SIZE(lowmem_adj);
-	int other_free = global_page_state(NR_FREE_PAGES);
+	int other_free = global_page_state(NR_FREE_PAGES) - totalreserve_pages;
 	int other_file = global_page_state(NR_FILE_PAGES) -
 						global_page_state(NR_SHMEM);
 	struct zone *zone;

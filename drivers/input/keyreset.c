21ac65a4ebe67036ebe7ca43c3be84fe231878a6
Hashcode <hashcode0f@gmail.com>
2013-03-03 21:55:52 -0800

    [moto] drivers/input/keyreset.c: changes

--- drivers/input/keyreset.c
+++ drivers/input/keyreset.c
@@ -19,12 +19,18 @@
 #include <linux/platform_device.h>
 #include <linux/reboot.h>
 #include <linux/sched.h>
-#include <linux/slab.h>
 #include <linux/syscalls.h>
+#include <linux/slab.h>
+#include <linux/spi/cpcap.h>
+#include <linux/spi/cpcap-regbits.h>
 
+
+#define KEYRESET_TIMER_DELAY 3500   /* ms */
+#define HWRESET_TIMER_DELAY 6000    /* ms */
 
 struct keyreset_state {
 	struct input_handler input_handler;
+	int crash_key;
 	unsigned long keybit[BITS_TO_LONGS(KEY_CNT)];
 	unsigned long upbit[BITS_TO_LONGS(KEY_CNT)];
 	unsigned long key[BITS_TO_LONGS(KEY_CNT)];
@@ -32,18 +38,54 @@ struct keyreset_state {
 	int key_down_target;
 	int key_down;
 	int key_up;
+	struct timer_list keyreset_timer;
+	struct timer_list hwreset_timer;
 	int restart_disabled;
 };
 
 int restart_requested;
+int crash_requested;
+
+static struct work_struct keyreset;
+static struct work_struct hwreset_set;
+static struct work_struct hwreset_clear;
+static struct workqueue_struct *_keyreset_wq;
+
 static void deferred_restart(struct work_struct *dummy)
 {
+	cpcap_set_bit(CPCAP_REG_VAL1,
+		CPCAP_BIT_PANIC, CPCAP_BIT_PANIC);
 	restart_requested = 2;
 	sys_sync();
 	restart_requested = 3;
-	kernel_restart(NULL);
+	BUG();
 }
-static DECLARE_WORK(restart_work, deferred_restart);
+
+static void keyreset_timer(unsigned long data)
+{
+	printk(KERN_EMERG "current process is %d:%s, prio is %d.\n",
+		current->pid, current->comm, current->prio);
+	dump_stack();
+	pr_info("keyboard reset\n");
+	queue_work(_keyreset_wq, &keyreset);
+	restart_requested = 1;
+}
+
+static void cpcap_set_hwreset(struct work_struct *dummy)
+{
+	cpcap_set_bit(CPCAP_REG_VAL1,
+		CPCAP_BIT_PANIC, CPCAP_BIT_PANIC);
+}
+
+static void cpcap_clear_hwreset(struct work_struct *dummy)
+{
+	cpcap_set_bit(CPCAP_REG_VAL1, 0, CPCAP_BIT_PANIC);
+}
+
+static void hwreset_timer(unsigned long data)
+{
+	queue_work(_keyreset_wq, &hwreset_set);
+}
 
 static void keyreset_event(struct input_handle *handle, unsigned int type,
 			   unsigned int code, int value)
@@ -56,6 +98,12 @@ static void keyreset_event(struct input_handle *handle, unsigned int type,
 
 	if (code >= KEY_MAX)
 		return;
+
+	if (code == state->crash_key) {
+		crash_requested = value;
+		if (!crash_requested && timer_pending(&state->keyreset_timer))
+			del_timer(&state->keyreset_timer);
+	}
 
 	if (!test_bit(code, state->keybit))
 		return;
@@ -78,18 +126,30 @@ static void keyreset_event(struct input_handle *handle, unsigned int type,
 	}
 	if (state->key_down == 0 && state->key_up == 0)
 		state->restart_disabled = 0;
-
 	pr_debug("reset key changed %d %d new state %d-%d-%d\n", code, value,
 		 state->key_down, state->key_up, state->restart_disabled);
 
 	if (value && !state->restart_disabled &&
-	    state->key_down == state->key_down_target) {
+	    state->key_down == state->key_down_target && crash_requested) {
 		state->restart_disabled = 1;
 		if (restart_requested)
 			panic("keyboard reset failed, %d", restart_requested);
-		pr_info("keyboard reset\n");
-		schedule_work(&restart_work);
-		restart_requested = 1;
+		mod_timer_pinned(&state->keyreset_timer, jiffies +
+			msecs_to_jiffies(KEYRESET_TIMER_DELAY));
+	}
+
+	if (value && state->key_down == state->key_down_target) {
+		mod_timer_pinned(&state->hwreset_timer, jiffies +
+			msecs_to_jiffies(HWRESET_TIMER_DELAY));
+	}
+
+	if (!value && ((state->key_down_target - state->key_down) == 1)) {
+		if (timer_pending(&state->keyreset_timer))
+			del_timer(&state->keyreset_timer);
+		if (timer_pending(&state->hwreset_timer))
+			del_timer(&state->hwreset_timer);
+		if (!timer_pending(&state->hwreset_timer))
+			queue_work(_keyreset_wq, &hwreset_clear);
 	}
 done:
 	spin_unlock_irqrestore(&state->lock, flags);
@@ -169,6 +229,7 @@ static int keyreset_probe(struct platform_device *pdev)
 	state = kzalloc(sizeof(*state), GFP_KERNEL);
 	if (!state)
 		return -ENOMEM;
+	state->crash_key = pdata->crash_key;
 
 	spin_lock_init(&state->lock);
 	keyp = pdata->keys_down;
@@ -187,6 +248,10 @@ static int keyreset_probe(struct platform_device *pdev)
 			__set_bit(key, state->upbit);
 		}
 	}
+	setup_timer(&state->keyreset_timer, keyreset_timer,
+		    (unsigned long)state);
+	setup_timer(&state->hwreset_timer, hwreset_timer,
+		    (unsigned long)state);
 	state->input_handler.event = keyreset_event;
 	state->input_handler.connect = keyreset_connect;
 	state->input_handler.disconnect = keyreset_disconnect;
@@ -218,11 +283,19 @@ struct platform_driver keyreset_driver = {
 
 static int __init keyreset_init(void)
 {
+	_keyreset_wq = create_singlethread_workqueue("keyreset");
+	if (!_keyreset_wq)
+		return 1;
+	INIT_WORK(&keyreset, deferred_restart);
+	INIT_WORK(&hwreset_set, cpcap_set_hwreset);
+	INIT_WORK(&hwreset_clear, cpcap_clear_hwreset);
 	return platform_driver_register(&keyreset_driver);
 }
 
 static void __exit keyreset_exit(void)
 {
+	flush_workqueue(_keyreset_wq);
+	destroy_workqueue(_keyreset_wq);
 	return platform_driver_unregister(&keyreset_driver);
 }
 

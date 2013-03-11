5223e2691f2d83b756dab322bbf4e58170c9e2f1
Hashcode <hashcode0f@gmail.com>
2013-03-11 00:00:50 -0700

    [moto] add security/crypto options: MODULE_EXTRA_COPY, CRYPTO_MOTOROLA_FAULT_INJECTION, CRYPTO_MOTOROLA_FIPS

--- kernel/module.c
+++ kernel/module.c
@@ -2741,6 +2741,9 @@ static struct module *load_module(void __user *umod,
 	struct load_info info = { NULL, };
 	struct module *mod;
 	long err;
+#ifdef CONFIG_MODULE_EXTRA_COPY
+	void *extra_copy = NULL;
+#endif
 
 	DEBUGP("load_module: umod=%p, len=%lu, uargs=%p\n",
 	       umod, len, uargs);
@@ -2749,6 +2752,17 @@ static struct module *load_module(void __user *umod,
 	err = copy_and_check(&info, umod, len, uargs);
 	if (err)
 		return ERR_PTR(err);
+
+#ifdef CONFIG_MODULE_EXTRA_COPY
+	/* Before we start modifying the module code to link it in, make an
+	   extra copy that will remain pristine. */
+	extra_copy = vmalloc(info.len);
+	if (extra_copy == NULL) {
+		err = -ENOMEM;
+		goto free_copy;
+	}
+	memcpy(extra_copy, info.hdr, info.len);
+#endif
 
 	/* Figure out module layout, and allocate all the memory. */
 	mod = layout_and_allocate(&info);
@@ -2834,9 +2848,20 @@ static struct module *load_module(void __user *umod,
 	if (err < 0)
 		goto unlink;
 
-	/* Get rid of temporary copy and strmap. */
+	/* Get rid of temporary strmap. */
 	kfree(info.strmap);
+
+#ifdef CONFIG_MODULE_EXTRA_COPY
+	/* Keep the linked copy as well as the raw copy, in case the
+	   module wants to inspect both. */
+	mod->raw_binary_ptr = extra_copy;
+	mod->raw_binary_size = info.len;
+	mod->linked_binary_ptr = info.hdr;
+	mod->linked_binary_size = info.len;
+#else
+	/* Get rid of temporary copy. */
 	free_copy(&info);
+#endif
 
 	/* Done! */
 	trace_module_load(mod);
@@ -2864,6 +2889,10 @@ static struct module *load_module(void __user *umod,
  free_module:
 	module_deallocate(mod, &info);
  free_copy:
+#ifdef CONFIG_MODULE_EXTRA_COPY
+	if (extra_copy != NULL)
+		vfree(extra_copy);
+#endif
 	free_copy(&info);
 	return ERR_PTR(err);
 }
@@ -2914,6 +2943,16 @@ SYSCALL_DEFINE3(init_module, void __user *, umod,
 	/* Start the module */
 	if (mod->init != NULL)
 		ret = do_one_initcall(mod->init);
+#ifdef CONFIG_MODULE_EXTRA_COPY
+	if (mod->raw_binary_ptr != NULL)
+		vfree(mod->raw_binary_ptr);
+	if (mod->linked_binary_ptr != NULL)
+		vfree(mod->linked_binary_ptr);
+	mod->raw_binary_ptr = NULL;
+	mod->linked_binary_ptr = NULL;
+	mod->raw_binary_size = 0;
+	mod->linked_binary_size = 0;
+#endif
 	if (ret < 0) {
 		/* Init routine failed: abort.  Try to protect us from
                    buggy refcounters. */

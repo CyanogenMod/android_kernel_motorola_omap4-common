b86bf8ddfef997998e38809a6638c45106697cf7
Michael Gernoth <michael@gernoth.net>
2014-05-18 01:00:47 +0200

    omap4-common: allow mediaserver to open a CAIF socket
    Change-Id: Idf4f6a03d1a8ef72e233b3f806e7471cc29b7752

--- net/caif/caif_socket.c
+++ net/caif/caif_socket.c
@@ -1070,7 +1070,8 @@ static int caif_create(struct net *net, struct socket *sock, int protocol,
 		.obj_size = sizeof(struct caifsock),
 	};
 
-	if (!capable(CAP_SYS_ADMIN) && !capable(CAP_NET_ADMIN) && !in_egroup_p(AID_MOT_CAIF))
+	/* mediaserver is in AID_NET_BT_ADMIN */
+	if (!capable(CAP_SYS_ADMIN) && !capable(CAP_NET_ADMIN) && !in_egroup_p(AID_MOT_CAIF) && !in_egroup_p(AID_NET_BT_ADMIN))
 		return -EPERM;
 	/*
 	 * The sock->type specifies the socket type to use.

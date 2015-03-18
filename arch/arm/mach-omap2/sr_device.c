e8f891e7c2e11a3b1cefb5b7252415eb114ef1df
JooJooBee666 <desire4homicide@gmail.com>
2015-03-18 13:07:22 -0400

    mach-omap2: Cleanup formatting

--- arch/arm/mach-omap2/sr_device.c
+++ arch/arm/mach-omap2/sr_device.c
@@ -71,13 +71,11 @@ static void __init sr_set_nvalues(struct omap_volt_data *volt_data,
 		
 		if (v == 0 || volt_data[i].nvalue_override > 0) {
 			/*
-			* If nvalue == 0 or nvalue_overide set, check our volt_data for an nvalue override.
+			* If v == 0 or nvalue_overide set, check our volt_data for an nvalue override.
 			*/
-			if (volt_data[i].nvalue_override > 0) {
-				printk(">>> sr_set_nvalues overriding v = %lu with %lu\n", v, volt_data[i].nvalue_override);
-				pr_info(">>> sr_set_nvalues overriding v = %lu with %lu\n", v, volt_data[i].nvalue_override);
-				v = volt_data[i].nvalue_override;			
-			}
+			printk(">>> sr_set_nvalues overriding v = %lu with %lu\n", v, volt_data[i].nvalue_override);
+			pr_info(">>> sr_set_nvalues overriding v = %lu with %lu\n", v, volt_data[i].nvalue_override);
+			v = volt_data[i].nvalue_override;
 		}
 		nvalue_table[i].efuse_offs = volt_data[i].sr_efuse_offs;
 		nvalue_table[i].nvalue = v;

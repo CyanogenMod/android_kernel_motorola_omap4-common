578aa7e19dbeb03555891f045b238e4d73dc3c2c
JooJooBee666 <desire4homicide@gmail.com>
2015-03-18 13:09:02 -0400

    Add Governors:  Add smartassH3 and InteractiveX governor support

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -364,6 +364,12 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_HOTPLUG)
 extern struct cpufreq_governor cpufreq_gov_hotplug;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_hotplug)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
+extern struct cpufreq_governor cpufreq_gov_interactivex;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactiveX)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASSH3)
+extern struct cpufreq_governor cpufreq_gov_smartassh3;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartassh3)
 #endif
 
 

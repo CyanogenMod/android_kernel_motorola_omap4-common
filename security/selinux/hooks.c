673c11a90c4b69c5002a8a2bfb0249902877d194
Michael Gernoth <michael@gernoth.net>
2015-12-18 09:43:49 +0100

    Revert "selinux: workaround for moto blobs"
    Nick Kralevich provided a way better solution to fix this. Thanks :-)
    
    This reverts commit b4bd4d9576969a3ab94bd17d60658bae5c26b773.
    
    Change-Id: Ic2840539501c79ec810bd0f5382f4673fdb3c59f

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -3227,12 +3227,7 @@ static int selinux_file_mprotect(struct vm_area_struct *vma,
 			 * modified content.  This typically should only
 			 * occur for text relocations.
 			 */
-			 /* STARGO: Hack for Moto blobs */
-			if (strcmp(current->comm, "battd") &&
-			    strcmp(current->comm, "msp430") &&
-			    strcmp(current->comm, "thermaldaemon") &&
-			    strcmp(current->comm, "whisperd"))
-				rc = file_has_perm(cred, vma->vm_file, FILE__EXECMOD);
+			rc = file_has_perm(cred, vma->vm_file, FILE__EXECMOD);
 		}
 		if (rc)
 			return rc;
